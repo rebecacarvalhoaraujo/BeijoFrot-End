b3e460213b8c1d0a817b1187da5ec36a8fefab7f
Becca <112990922+rebecacarvalhoaraujo@users.noreply.github.com>
2023-04-26 17:39:40 -0300

    Atualizações

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,9 @@
-import { Link, Outlet } from "react-router-dom";
-
-import banner from "../../assets/banner-home.png"
+import banner from "../../assets/banner-home.png";
 
 export function Home() {
-    return (
-        <div className="home">
-            <img src={banner} alt="Banner SoulPet" className="w-100" />
-        </div>
-    )
+  return (
+    <div className="home">
+      <img src={banner} alt="Banner SoulPet" className="w-100" />
+    </div>
+  );
 }
